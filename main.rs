c09d8ece46a78e61fdf85744a043ff916a669005
RachitVargas <65442367+RachitVargas@users.noreply.github.com>
2020-11-15 22:08:42 -0600

    Update main.rs
    Se hicieron cambios

--- main.rs
+++ main.rs
@@ -1,60 +1,75 @@
-use std::io;
 
 fn main() {
 
-    menu();
-    let select_option:u32 = selectOp();
-    let mut message:String = String::new();
-    let mut key:String = String::new();
+    //MESSAGE
+    let message = "HOLAMUNDOY";
+
+    // ALPHABET
+    let alphabet:[char; 26] = [
+        'A', 'B', 'C', 'D', 'E',
+        'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y',
+        'Z'
+    ];
+
+    let _ciphertex:String = encryp(message, alphabet);
+    println!("This is the ciphertext: {} ",_ciphertex);
+    let _message_decryp:String = decryp(_ciphertex, alphabet);
+    println!("This is the message: {} ", _message_decryp);
+
+}
+
+fn encryp(_message:&str, _alphabet:[char;26]) -> String{
+
+    let message_char: Vec<char> = _message.chars().collect();
     let mut ciphertext:String = String::new();
-    let mut message_decryp: String = String::new();
 
-    if(select_option == 1){
-
-        message = enter_message();
-        println!("This is your message: {}", message);
-        ciphertext = encryp(message);
-
-    }else if(select_option == 2){
-
-        println!("This is the ciphertex: {} ", ciphertext);
-
-    }else if(select_option == 3){
-
-        message_decryp = decryp();
-
-    }else{
-        println!("Sorry, we have a problem with the option selected");
+    let mut i = 0;
+    while i != message_char.len() {
+        let mut count = 0;
+        let mut j = 0;
+        while j != _alphabet.len() {
+            if message_char[i] != _alphabet[count] {
+                count = count + 1;
+            }
+            j = j+1;
+        }
+        if count < 25 {
+            count = count + 1;
+            ciphertext.push(_alphabet[count].to_string().parse().unwrap());
+        }else{
+            ciphertext.push(_alphabet[0].to_string().parse().unwrap());
+        }
+         i = i+1;
     }
+
+    return ciphertext;
 }
 
-fn decryp() -> String{
-    let me: &str = "Hi";
-    return (me).parse().unwrap();
-
-}
-
-fn encryp(message:String) -> String{
-    let me: &str = "Hi";
-    return (me).parse().unwrap();
-}
-
-fn enter_message() -> String{
-    println!("Enter the message");
+fn decryp (_ciphertext:String,_alphabet:[char; 26]) -> String {
+    let ciphertext_char: Vec<char> = _ciphertext.chars().collect();
     let mut message:String = String::new();
-    io::stdin().read_line(&mut message);
-    return (message);
-}
 
-fn selectOp() -> u32 {
-    let mut option = String::new();
-    io::stdin().read_line(&mut option);
-    let option:u32 = option.trim().parse().unwrap();
-    return (option);
-}
+    let mut i = 0;
+    while i != ciphertext_char.len() {
+        let mut count = 0;
+        let mut j = 0;
+        while j != _alphabet.len() {
+            if ciphertext_char[i] != _alphabet[count] {
+                count = count + 1;
+            }
+            j = j+1;
+        }
+        if count < 25 || count > 0 {
+            count = count - 1;
+            message.push(_alphabet[count].to_string().parse().unwrap());
+        }else{
+            message.push(_alphabet[25].to_string().parse().unwrap());
+        }
+        i = i+1;
+    }
 
-fn menu(){
-    println!("Hi, welcome to enigma code!");
-    println!("Please choose a option");
-    println!("1. Enconding a message\n2. Get the ciphertex\n3. Get the message");
+    return message;
 }
