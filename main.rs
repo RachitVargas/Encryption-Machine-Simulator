01ed3967cc2e66fc71b3588c96824008624f259d
RachitVargas <65442367+RachitVargas@users.noreply.github.com>
2020-11-16 22:17:31 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,11 +1,9 @@
+use std::io;
 
 fn main() {
+    let message = "HELLOWORLD";
 
-    //MESSAGE
-    let message = "HOLAMUNDOY";
-
-    // ALPHABET
-    let alphabet:[char; 26] = [
+    let alphabet: [char; 26] = [
         'A', 'B', 'C', 'D', 'E',
         'F', 'G', 'H', 'I', 'J',
         'K', 'L', 'M', 'N', 'O',
@@ -14,17 +12,15 @@ fn main() {
         'Z'
     ];
 
-    let _ciphertex:String = encryp(message, alphabet);
-    println!("This is the ciphertext: {} ",_ciphertex);
-    let _message_decryp:String = decryp(_ciphertex, alphabet);
+    let _ciphertex: String = encryp(message, alphabet);
+    println!("This is the ciphertext: {} ", _ciphertex);
+    let _message_decryp: String = decryp(_ciphertex, alphabet);
     println!("This is the message: {} ", _message_decryp);
-
 }
 
-fn encryp(_message:&str, _alphabet:[char;26]) -> String{
-
+fn encryp(_message: &str, _alphabet: [char; 26]) -> String {
     let message_char: Vec<char> = _message.chars().collect();
-    let mut ciphertext:String = String::new();
+    let mut ciphertext: String = String::new();
 
     let mut i = 0;
     while i != message_char.len() {
@@ -34,7 +30,7 @@ fn encryp(_message:&str, _alphabet:[char;26]) -> String{
             if message_char[i] != _alphabet[count] {
                 count = count + 1;
             }
-            j = j+1;
+            j = j + 1;
         }
         if count < 25 {
             count = count + 1;
@@ -42,15 +38,14 @@ fn encryp(_message:&str, _alphabet:[char;26]) -> String{
         }else{
             ciphertext.push(_alphabet[0].to_string().parse().unwrap());
         }
-         i = i+1;
+        i = i+1;
     }
-
     return ciphertext;
 }
 
-fn decryp (_ciphertext:String,_alphabet:[char; 26]) -> String {
+fn decryp(_ciphertext: String, _alphabet: [char; 26]) -> String {
     let ciphertext_char: Vec<char> = _ciphertext.chars().collect();
-    let mut message:String = String::new();
+    let mut message: String = String::new();
 
     let mut i = 0;
     while i != ciphertext_char.len() {
@@ -60,16 +55,29 @@ fn decryp (_ciphertext:String,_alphabet:[char; 26]) -> String {
             if ciphertext_char[i] != _alphabet[count] {
                 count = count + 1;
             }
-            j = j+1;
+            j = j + 1;
         }
-        if count < 25 || count > 0 {
-            count = count - 1;
-            message.push(_alphabet[count].to_string().parse().unwrap());
-        }else{
+        if count == 0 {
             message.push(_alphabet[25].to_string().parse().unwrap());
+        }else{
+            count = count-1;
+            message.push(_alphabet[count].to_string().parse().unwrap());
         }
-        i = i+1;
+        i = i + 1;
     }
-
     return message;
+}
+
+fn rotor (count:usize)-> usize{
+
+    println!("What rotor do you want?\n1. rotor 1\n2. rotor 2");
+    let opcion_1:u32;
+    let mut opcion= String::new();
+    io::stdin().read_line(&mut opcion);
+    opcion_1 = opcion.trim().parse().unwrap();
+
+    if opcion_1 == 1{
+        return count+1
+    }
+    return count-1;
 }
